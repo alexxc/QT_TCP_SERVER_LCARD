c548602e4edc3234e45dcf56deca169fa36f0a40
nowhard <nowhard@mail.ru>
2016-12-22 17:13:04 +0300

    Необходимо реализовать цепочку ответов на запросы к FPGA

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,14 +2,34 @@
 #include "ui_mainwindow.h"
 
 
-static qint32 f_cmdthread_run;
-static qint32 f_dataconn;
-static quint32 f_ethcommand[ 128 ];
-static qint32 f_init_require;
+QDataStream &operator<<(QDataStream &out, const t_e502_tcp_resp_hdr &tcp_resp_hdr)
+{
+    out <<tcp_resp_hdr.sign;
+    out <<tcp_resp_hdr.res;
+    out <<tcp_resp_hdr.len;
 
-//quint8 g_altera_info[ 510 ];
+    return out;
+}
 
-t_lboot_devinfo devinfo;
+QDataStream &operator<<(QDataStream &out, const t_lboot_devinfo &lboot_devinfo)
+{
+//    out.writeBytes(lboot_devinfo.devname,LBOOT_DEVNAME_SIZE);
+//    out.writeBytes(lboot_devinfo.serial,LBOOT_SERIAL_SIZE);
+//    out.writeBytes(lboot_devinfo.soft_ver,LBOOT_SOFTVER_SIZE);
+//    out.writeBytes(lboot_devinfo.brd_revision,LBOOT_REVISION_SIZE);
+//    out.writeBytes(lboot_devinfo.brd_impl,LBOOT_IMPLEMENTATION_SIZE);
+//    out.writeBytes(lboot_devinfo.spec_info,LBOOT_SPECINFO_SIZE);
+
+    out.writeRawData(lboot_devinfo.devname,LBOOT_DEVNAME_SIZE);
+    out.writeRawData(lboot_devinfo.serial,LBOOT_SERIAL_SIZE);
+    out.writeRawData(lboot_devinfo.soft_ver,LBOOT_SOFTVER_SIZE);
+    out.writeRawData(lboot_devinfo.brd_revision,LBOOT_REVISION_SIZE);
+    out.writeRawData(lboot_devinfo.brd_impl,LBOOT_IMPLEMENTATION_SIZE);
+    out.writeRawData(lboot_devinfo.spec_info,LBOOT_SPECINFO_SIZE);
+
+    return out;
+}
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -17,18 +37,15 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    f_cmdthread_run = 0;
-    f_dataconn = 0;
-    f_init_require = 0;
-
-
-//    for (quint16 i = 0; (i < 510); i += 2)
-//     {
-//        g_altera_info[ i ]   = 0x00;
-//        g_altera_info[ i+1 ] = 0xE0;
-//     }
-
     strcpy(devinfo.devname,E502_DEVICE_NAME);
+    strcpy(devinfo.serial,E502_DEVICE_SERIAL);
+//    strcpy(devinfo.soft_ver,"1");
+//    strcpy(devinfo.brd_impl,"1");
+//    strcpy(devinfo.brd_revision,"1");
+//    strcpy(devinfo.spec_info,"1");
+
+    devflags=(X502_DEVFLAGS_IFACE_SUPPORT_ETH | X502_DEVFLAGS_INDUSTRIAL | X502_DEVFLAGS_FPGA_LOADED);
+    bf_ctl=0x0;
 }
 
 MainWindow::~MainWindow()
@@ -86,18 +103,16 @@ void MainWindow::slotReadClient()
 {
     QByteArray data;
     QTcpSocket* clientSocket = (QTcpSocket*)sender();
-//    int idusersocs=clientSocket->socketDescriptor();
- //   QTextStream os(clientSocket);
-//
-
+    int idusersocs=clientSocket->socketDescriptor();
 
     data=clientSocket->readAll();
+    ui->textinfo->append("ReadClient:"+ data +"\n\r");
+
     processCommand(clientSocket,data);
 
-    ui->textinfo->append("ReadClient:"+ data +"\n\r");
     // Если нужно закрыть сокет
-    //clientSocket->close();
-    //SClients.remove(idusersocs);
+  //  clientSocket->close();
+  //  SClients.remove(idusersocs);
 }
 
 void MainWindow::sendCommand(
@@ -109,46 +124,48 @@ const quint8 *data                     /* (вх) - передаваемые да
 {
 
     QDataStream sendStream(clientSocket);
+
     t_e502_tcp_resp_hdr e502_tcp_resp_hdr;
+    sendStream.setByteOrder(QDataStream::LittleEndian);
+
     e502_tcp_resp_hdr.sign = SIGNATURE;
     e502_tcp_resp_hdr.res  = cmd_code;
     e502_tcp_resp_hdr.len  = len;
 
+    sendStream<<e502_tcp_resp_hdr;
 
-    sendStream.writeBytes((const char *)&e502_tcp_resp_hdr,sizeof(e502_tcp_resp_hdr));
     if ((len > 0) && (data != NULL))
-       sendStream.writeBytes((const char *)data,len);
+       sendStream.writeRawData((const char *)data,len);
 }
 
 void MainWindow::processCommand(QTcpSocket* clientSocket, QByteArray data)
 {
 
 quint32 *command_buf=(quint32*)data.data();
+QByteArray devinfo_data;
+QDataStream DevInfoStream(&devinfo_data,QIODevice::ReadWrite);
+DevInfoStream.setByteOrder(QDataStream::LittleEndian);
+
 switch (command_buf[ ETHREQIDX_CMDCODE ])
     {
-    case ETHCMD_INIT:
-        if (f_dataconn)
-            f_init_require = 1;
-        sendCommand( clientSocket, ETHRETCODE_OK, sizeof(devinfo), (const quint8 *)&devinfo);
+    case E502_CM4_CMD_GET_MODULE_INFO:
+        DevInfoStream<<devinfo;
+        sendCommand( clientSocket, ETHRETCODE_OK, devinfo_data.size(), (const quint8 *)devinfo_data.data());
         break;
-    case ETHCMD_NOP:
-        sendCommand( clientSocket, ETHRETCODE_OK, 0, NULL );
+
+    case E502_CM4_CMD_GET_DEVFLAGS:
+        sendCommand( clientSocket, ETHRETCODE_OK, sizeof(devflags), (const quint8 *)&devflags );
         break;
-    case ETHCMD_GETNAME:
+
+    case E502_CM4_CMD_GET_MODULE_NAME:
         sendCommand( clientSocket, ETHRETCODE_OK, LBOOT_DEVNAME_SIZE, (const quint8 *)devinfo.devname );
         break;
-    case ETHCMD_GETARRAY:
-//        f_get_array_cmd( clientSocket, command_buf[ ETHREQIDX_CMDPARAM ] >> 24,
-//            command_buf[ ETHREQIDX_CMDPARAM ] & 0xFFFFFF, command_buf[ ETHREQIDX_REPLYLEN ] );
 
-            sendCommand( clientSocket, ETHRETCODE_OK, 0, NULL );
+    case E502_CM4_CMD_FPGA_REG_READ:
+        sendCommand( clientSocket, ETHRETCODE_OK, sizeof(bf_ctl), (const quint8 *)&bf_ctl );
         break;
-    case ETHCMD_PUTARRAY:
-//        f_put_array_cmd( clientSocket, command_buf[ ETHREQIDX_CMDPARAM ] >> 24,
-//            command_buf[ ETHREQIDX_CMDPARAM ] & 0xFFFFFF, command_buf[ ETHREQIDX_DATALEN ] );
-          sendCommand( clientSocket, ETHRETCODE_OK, 0, NULL );
-        break;
-    default:
-        break;
+
     }
 }
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -37,11 +37,81 @@
 #define ETHRETCODE_OK (0)
 #define ETHRETCODE_ERROR (1)
 
-#define ETHCMD_NOP (0)
-#define ETHCMD_GETNAME (0x00000001)
-#define ETHCMD_GETARRAY (0x00000002)
-#define ETHCMD_PUTARRAY (0x00000003)
-#define ETHCMD_INIT (0x00000080)//(0x80000000)
+typedef enum {
+    E502_CM4_CMD_GET_MODULE_NAME        = 11,
+    E502_CM4_CMD_GET_USB_SPEED          = 6,
+
+    E502_CM4_CMD_BOOT                   = 0x0F,
+    E502_CM4_CMD_FPGA_REG_READ          = 0x10,
+    E502_CM4_CMD_FPGA_REG_WRITE         = 0x11,
+    E502_CM4_CMD_STREAM_START           = 0x12,
+    E502_CM4_CMD_STREAM_STOP            = 0x13,
+    E502_CM4_CMD_STREAM_SET_STEP        = 0x14,
+    E502_CM4_CMD_STREAM_IS_RUNNING      = 0x15,
+    E502_CM4_CMD_FIRM_BUF_WRITE         = 0x16,
+    E502_CM4_CMD_FLASH_RD               = 0x17,
+    E502_CM4_CMD_FLASH_WR               = 0x18,
+    E502_CM4_CMD_FLASH_ERASE            = 0x19,
+    E502_CM4_CMD_FLASH_SET_PROTECT      = 0x1A,
+    E502_CM4_CMD_FLASH_RELOAD_INFO      = 0x1B,
+    E502_CM4_CMD_ETH_CFG_SET            = 0x1C,
+    E502_CM4_CMD_ETH_CFG_GET            = 0x1D,
+    E502_CM4_CMD_BF_MEM_WRITE           = 0x20,
+    E502_CM4_CMD_BF_MEM_READ            = 0x21,
+    E502_CM4_CMD_BF_FIRM_LOAD           = 0x22,
+    E502_CM4_CMD_DROP_DATA_CON          = 0x23,
+    E502_CM4_CMD_RELOAD_FPGA            = 0x24,
+    E502_CM4_CMD_GET_DEVFLAGS           = 0x25,
+
+    E502_CM4_CMD_OUT_CYCLE_LOAD         = 0x26,
+    E502_CM4_CMD_OUT_CYCLE_SETUP        = 0x27,
+    E502_CM4_CMD_OUT_CYCLE_STOP         = 0x28,
+    E502_CM4_CMD_OUT_CYCLE_SETUP_CHECK  = 0x29,
+
+
+
+    E502_CM4_CMD_TEST_START             = 0x40,
+    E502_CM4_CMD_TEST_STOP              = 0x41,
+    E502_CM4_CMD_TEST_GET_STATE         = 0x42,
+
+    E502_CM4_CMD_GET_MODULE_INFO        = 0x80,
+    E502_CM4_CMD_GET_MODULE_MODE        = 0x81,
+    E502_CM4_CMD_GET_LAST_ERROR         = 0x82
+} t_e502_cm4_cmd_codes;
+
+
+typedef enum {
+    /** Признак наличия двухканального канального ЦАП */
+    X502_DEVFLAGS_DAC_PRESENT           = 0x00000001,
+    /** Признак наличия гальваноразвязки */
+    X502_DEVFLAGS_GAL_PRESENT           = 0x00000002,
+    /** Признак наличия сигнального процессора BlackFin */
+    X502_DEVFLAGS_BF_PRESENT            = 0x00000004,
+
+    /** Признак, что устройство поддерживает интерфейс USB */
+    X502_DEVFLAGS_IFACE_SUPPORT_USB     = 0x00000100,
+    /** Признак, что устройство поддерживает Ethernet */
+    X502_DEVFLAGS_IFACE_SUPPORT_ETH     = 0x00000200,
+    /** Признак, что устройство поддерживает интерфейс PCI/PCI-Express */
+    X502_DEVFLAGS_IFACE_SUPPORT_PCI     = 0x00000400,
+
+    /** Признак, что устройство выполнено в индустриалном исполнении */
+    X502_DEVFLAGS_INDUSTRIAL            = 0x00008000,
+
+    /** Признак, что во Flash-памяти присутствует информация о модуле */
+    X502_DEVFLAGS_FLASH_DATA_VALID      = 0x00010000,
+    /** Признак, что во Flash-памяти присутствуют действительные калибровочные
+        коэффициенты АЦП */
+    X502_DEVFLAGS_FLASH_ADC_CALIBR_VALID = 0x00020000,
+    /** Признак, что во Flash-памяти присутствуют действительные калибровочные
+        коэффициенты ЦАП */
+    X502_DEVFLAGS_FLASH_DAC_CALIBR_VALID = 0x00040000,
+
+    /** Признак, что присутствует прошивка ПЛИС и она успешно была загружена */
+    X502_DEVFLAGS_FPGA_LOADED            = 0x00800000,
+    /** Признак, что устройство уже открыто (действителен только внутри t_x502_devrec) */
+    X502_DEVFLAGS_DEVREC_OPENED          = 0x01000000
+} t_x502_dev_flags;
 
 
 #define DESC_MODULE_NAME_SIZE          (16)
@@ -52,6 +122,7 @@
 
 
 #define E502_DEVICE_NAME "E502"
+#define E502_DEVICE_SERIAL "2T222563"
 
 
 #define LBOOT_DEVNAME_SIZE            32
@@ -62,7 +133,7 @@
 #define LBOOT_SPECINFO_SIZE           64
 
 
-#pragma pack(push, 1)
+//#pragma pack(push, 1)
 struct lboot_devinfo_st {
     char devname[LBOOT_DEVNAME_SIZE]; /**< название устройства */
     char serial[LBOOT_SERIAL_SIZE];   /**< серийный номер */
@@ -74,13 +145,13 @@ struct lboot_devinfo_st {
 typedef struct lboot_devinfo_st t_lboot_devinfo;
 
 
-typedef struct {
+struct t_e502_tcp_resp_hdr_st{
     quint32 sign;
     quint32 res;  //код возврата (0 - ОК, <0 - ошибка)
     quint32 len; //кол-во данных в ответе
-} t_e502_tcp_resp_hdr;
-
-#pragma pack(pop)
+} ;
+typedef struct t_e502_tcp_resp_hdr_st t_e502_tcp_resp_hdr;
+//#pragma pack(pop)
 
 namespace Ui {
     class MainWindow;
@@ -107,7 +178,9 @@ private:
     QTcpServer *tcpServer;
     int server_status;
     QMap<int,QTcpSocket *> SClients;
-
+    quint32 devflags;
+    t_lboot_devinfo devinfo;
+    quint32 bf_ctl;
 
 
     void sendCommand(QTcpSocket* clientSocket, quint32 cmd_code, quint32 len, const quint8 *data);
